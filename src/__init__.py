2128e0f1145bba99624367f607adc3cd85376f98
Duy-sketch <doubleduypro@gmail.com>
2026-09-14 22:14:46 +0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,8 @@
+DEMA-MCE magnetic capsule control simulation.
 
+This package contains:
+- magnetic field and wrench models
+- capsule/fluid dynamics
+- control algorithms
+- sensing models
+- MuJoCo simulation utilities
