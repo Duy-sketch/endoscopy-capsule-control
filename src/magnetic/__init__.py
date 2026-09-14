ed5fe496bf06c3b7de6c35b4c63634b770591109
Duy-sketch <doubleduypro@gmail.com>
2026-09-14 22:20:57 +0700

    Create __init__.py

---
+++ src/magnetic/__init__.py
@@ -0,0 +1,34 @@
+"""
+Magnetic models used by the DEMA-MCE simulation.
+"""
+
+from .capsule_magnet import (
+    angle_between_deg,
+    magnetic_potential_energy,
+    moment_world_from_body,
+    unit,
+)
+
+from .dipole import (
+    MU0,
+    dipole_field,
+    total_field,
+)
+
+from .wrench import (
+    force_fixed_capsule_moment,
+    magnetic_wrench,
+)
+
+
+__all__ = [
+    "MU0",
+    "unit",
+    "dipole_field",
+    "total_field",
+    "moment_world_from_body",
+    "magnetic_potential_energy",
+    "force_fixed_capsule_moment",
+    "magnetic_wrench",
+    "angle_between_deg",
+]
